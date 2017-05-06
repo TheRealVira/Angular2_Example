3351ca9d594d3003d8893c0444a0e4a6f84f5efe
TheRealVira <Thomas.Ruehrig@studierende.htl-donaustadt.at>
2017-05-06 19:16:32 +0200

    Added some nice features to visualise the power of typescript and Angular 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Input,Component,OnChanges } from '@angular/core';
 
 import '../js/test.js';
+
+const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnChanges {
+	text = '';
+
+	oldLength = 10;
+	length = 10;
+	
+	constructor(){
+		this.generate();
+	}
+	
+	ngOnChanges(changeRecord) {
+		if(length==42){
+			alert(':)');
+		}
+	}
+
+	ngDoCheck(){
+		if(this.oldLength!=this.length){
+			this.oldLength = this.length;
+			
+			if(this.length==42){
+				console.log(':)');
+			}
+		}
+	}
+
+	generate(){
+		this.text = '';
+		
+		for( var i=0; i < this.length; i++ )
+        	this.text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
 }
